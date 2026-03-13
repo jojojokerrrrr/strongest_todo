7b80df7e744da9836541c9f46fcbdd360961d3c8
jojojoker893 <jojojoker114514@gmail.com>
2026-03-14 08:11:20 +0900

    tasksのfactoryの修正

--- backend/spec/factories/tasks.rb
+++ backend/spec/factories/tasks.rb
@@ -2,27 +2,18 @@ FactoryBot.define do
   factory :task do
     title { "task_name" }
     description { "task_description" }
-    category { "task_category" }
     status { :incomplete }
     visibility { :public_task }
     association :user
+    association :category
 
     trait :other_task do
       title { "other_task_name" }
       description { "other_task_description" }
-      category { "other_task_category" }
       status { :incomplete }
       visibility { :private_task }
       association :user, :other_user
-    end
-
-    trait :invalid_task do
-      title { " " }
-      description { "invalid" }
-      category { " " }
-      status { :incomplete }
-      visibility { :public_task }
-      association :user
+      association :category
     end
   end
 end
