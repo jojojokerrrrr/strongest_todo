4db65bb8f24c55fc04f6c782692f246e94247f72
jojojoker893 <jojojoker114514@gmail.com>
2026-03-14 13:01:41 +0900

    rubocopでlint修正

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -2,9 +2,9 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :title, presence: true, length: {maximum: 50}
+  validates :title, presence: true, length: { maximum: 50 }
   validates :category, presence: true
-  validates :description, length: {maximum: 500}, allow_blank: true
+  validates :description, length: { maximum: 500 }, allow_blank: true
 
   enum :status, { incomplete: 0, completed: 1 }
 end
--- backend/spec/models/task_spec.rb
+++ backend/spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Task, type: :model do
   describe "validations" do
-    let(:task) {build(:task)}
+    let(:task) { build(:task) }
 
     context "全て有効な場合" do
       it "保存に成功すること" do
@@ -11,28 +11,28 @@ RSpec.describe Task, type: :model do
     end
 
     context "titleが50文字を超える場合" do
-      let(:task) {build(:task, title: "a" * 51)}
+      let(:task) { build(:task, title: "a" * 51) }
       it "保存できないこと" do
-        expect(task).to be_invalid  
+        expect(task).to be_invalid
       end
     end
-    
+
     context "descriptionが500文字を超える場合" do
-      let(:task) {build(:task, description: "a" * 501)}
+      let(:task) { build(:task, description: "a" * 501) }
       it "保存できないこと" do
-        expect(task).to be_invalid  
+        expect(task).to be_invalid
       end
     end
 
     context "titleがない場合" do
-      let(:task) {build(:task, title: "")}
+      let(:task) { build(:task, title: "") }
       it "保存できないこと" do
-        expect(task).to be_invalid 
+        expect(task).to be_invalid
       end
     end
 
     context "カテゴリーがない場合" do
-      let(:task) {build(:task, category: nil)}
+      let(:task) { build(:task, category: nil) }
       it "保存できないこと" do
         expect(task).to be_invalid
       end
@@ -40,11 +40,11 @@ RSpec.describe Task, type: :model do
   end
 
   describe "enum" do
-    let(:task) {build(:task)}
+    let(:task) { build(:task) }
 
     context "statusのデフォルト" do
       it "incompleteであること" do
-        expect(task.status).to eq ("incomplete")  
+        expect(task.status).to eq ("incomplete")
       end
     end
   end
