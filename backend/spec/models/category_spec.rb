f786699a07fb8dbfb3a7ce311386d3fa185e8468
jojojoker893 <jojojoker114514@gmail.com>
2026-03-16 18:01:24 +0900

    lint修正

--- backend/spec/models/category_spec.rb
+++ backend/spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe "validation" do
-    let(:category) {build(:category)}
+    let(:category) { build(:category) }
     context "nameが入力されている場合" do
       it "保存できること" do
         expect(category).to be_valid
@@ -10,7 +10,7 @@ RSpec.describe Category, type: :model do
     end
 
     context "nameが入力されていない場合" do
-      let(:invalid_category) {build(:category, name: "")}
+      let(:invalid_category) { build(:category, name: "") }
       it "保存できないこと" do
         expect(invalid_category).not_to be_valid
       end
