43e6c475353c226eadc84257757e7d86bd3edef7
jojojoker893 <jojojoker114514@gmail.com>
2026-03-16 17:54:49 +0900

    categorymodelのバリデーション追加しmodelspecの記述

--- backend/spec/models/category_spec.rb
+++ backend/spec/models/category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    let(:category) {build(:category)}
+    context "nameが入力されている場合" do
+      it "保存できること" do
+        expect(category).to be_valid
+      end
+    end
+
+    context "nameが入力されていない場合" do
+      let(:invalid_category) {build(:category, name: "")}
+      it "保存できないこと" do
+        expect(invalid_category).not_to be_valid
+      end
+    end
+  end
 end
